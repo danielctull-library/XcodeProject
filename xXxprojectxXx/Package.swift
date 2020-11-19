a9798d03a3c1a3631dfec2bf270ef4b062ad7d4f
Daniel Tull <dt@danieltull.co.uk>
2020-11-19 15:11:47 +0000

    Add swift package to manage shared code

---
+++ xXxprojectxXx/Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "xXxprojectxXx",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v11),
+        .tvOS(.v14),
+        .watchOS(.v7),
+    ],
+    products: [
+        .library(name: "xXxprojectxXxKit", targets: ["xXxprojectxXxKit"]),
+        .library(name: "xXxprojectxXxUI", targets: ["xXxprojectxXxUI"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+
+        .target(name: "xXxprojectxXxKit"),
+
+        .target(
+            name: "xXxprojectxXxUI",
+            dependencies: [
+                "xXxprojectxXxKit",
+            ]),
+    ]
+)
